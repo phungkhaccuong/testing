1dafeaf7d6b6d9e97ec4762d5fc94959df04cac7
chauhm <kidphys@gmail.com>
2024-03-22 22:34:13 +0700

    Update version

--- base_miner/predict.py
+++ base_miner/predict.py
@@ -36,7 +36,7 @@ def simple_predict(timestamp:datetime, scaler:MinMaxScaler, model, type) -> floa
 
     # data.to_csv('mining_models/base_miner_data.csv')
     input = matching_row[['Open', 'High', 'Low', 'Volume', 'SMA_50', 'SMA_200', 'RSI', 'CCI', 'Momentum', 'Close']]
-    prediction = input['Close'].iloc[0] + np.random.normal()
+    prediction = input['Close'].iloc[0] + np.random.normal(0, 0.5)
     return np.array([[prediction]])
 
 
